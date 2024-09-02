13d2fdaece34bd5e5ca3f64b5a3f42d3e981159d
SGVp15 <phosphorusp15@mail.ru>
2024-09-02 11:58:28 +0300

    log

--- main.py
+++ main.py
@@ -16,11 +16,12 @@ def daterange(start_date: date, end_date: date):
 
 def create_dirs(course: Course):
     path_course = os.path.join(yandex_dir, str(course))
-    for single_date in daterange(course.date_start, course.date_stop):
-        path_full = os.path.join(path_course, single_date.strftime("%Y-%m-%d"))
-        if not os.path.exists(path_full):
-            os.makedirs(path_full, exist_ok=True)
-            log.info(f'[CREATE] {path_full}')
+    if not os.path.exists(path_course):
+        for single_date in daterange(course.date_start, course.date_stop):
+            path_full = os.path.join(path_course, single_date.strftime("%Y-%m-%d"))
+            if not os.path.exists(path_full):
+                os.makedirs(path_full, exist_ok=True)
+                log.info(f'[CREATE] {path_full}')
 
 
 def rename_old_dirs():
@@ -32,7 +33,7 @@ def rename_old_dirs():
             date_course_str = list(map(int, words[1].split('-')))
             date_end_course = datetime.date(date_course_str[0], date_course_str[1], date_course_str[2])
             today = datetime.date.today()
-            if date_end_course < today and is_empty_folders(os.path.join(yandex_dir, dir)) is False:
+            if date_end_course < today and is_empty_folders_in_path(os.path.join(yandex_dir, dir)) is False:
                 new_name = f'{words[2]} {words[3]} {words[4]} {words[0]}'
                 old_path = os.path.join(yandex_dir, dir)
                 os.rename(old_path, os.path.join(yandex_dir, new_name))
@@ -41,7 +42,7 @@ def rename_old_dirs():
             continue
 
 
-def is_empty_folders(path):
+def is_empty_folders_in_path(path):
     for root, dirs, files in os.walk(path):
         if not dirs and not files:
             return True
