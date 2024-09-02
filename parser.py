a0c44ec052b64981a437b06202581c211ca986ee
SGVp15 <phosphorusp15@mail.ru>
2024-09-02 11:43:11 +0300

    log

--- parser.py
+++ parser.py
@@ -20,8 +20,8 @@ def parse_for_course(s: str) -> [Course]:
             date_stop_str = elem[5].split('.')
             date_stop_str = list(map(int, date_stop_str))
 
-            date_start = datetime.date(2000 + date_start_str[2], date_start_str[1], date_start_str[0])
-            date_stop = datetime.date(2000 + date_stop_str[2], date_stop_str[1], date_stop_str[0])
+            date_start = datetime.date(date_start_str[2], date_start_str[1], date_start_str[0])
+            date_stop = datetime.date(date_stop_str[2], date_stop_str[1], date_stop_str[0])
             if re.findall(r'_\d', site):
                 courses.append(Course(name, date_start, date_stop, teacher, site))
         except IndexError as e:
