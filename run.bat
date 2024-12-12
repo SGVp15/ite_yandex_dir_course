e5300d35291fa4ba2e0015b8c4ef0d1aa76e30fc
SGVp15 <phosphorusp15@mail.ru>
2024-12-12 14:22:03 +0300

    course_name + date

--- run.bat
+++ run.bat
@@ -1,3 +1,5 @@
-rem git pull https://github.com/SGVp15/ite_yandex_dir_course
-rem pip install -r requirements.txt
+git pull https://github.com/SGVp15/ite_yandex_dir_course
+pip install -r requirements.txt
 python main.py
+
+@REM REM pause
